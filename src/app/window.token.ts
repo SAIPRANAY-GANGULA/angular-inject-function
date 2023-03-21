61213acf988f13dc88e5ffb797cd7815535383c1
SAIPRANAY-GANGULA <gangulasaipranay@gmail.com>
2023-03-21 21:46:14 +0530

    inject function was only used in factory before angular 14

--- src/app/window.token.ts
+++ src/app/window.token.ts
@@ -1,8 +1,10 @@
-import { InjectionToken } from "@angular/core";
+import {inject, InjectionToken, PLATFORM_ID} from "@angular/core";
 
 export const WINDOW = new InjectionToken<Window>('global Window object', {
   providedIn: 'root',
   factory: () => {
-    return window;
+    // inject function was only used in factory before angular 14
+    const platform = inject(PLATFORM_ID)
+    return platform === 'browser' ? window : {} as Window;
   }
 })
