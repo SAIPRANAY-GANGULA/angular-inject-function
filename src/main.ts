6b2664fa2d9407a9121a1f8418843f0384e2b2f0
SAIPRANAY-GANGULA <gangulasaipranay@gmail.com>
2023-03-21 22:32:42 +0530

    we need to create guards in separate files if logic is simple, just inject service to check

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, inject } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 import { environment } from './environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 if (environment.production) {
   enableProdMode();
 }
 
+import { Routes } from '@angular/router';
+import { LoginService } from './app/login.service';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./app/app.component').then((m) => m.AppComponent),
+    // we need to create guards in separate files if logic is simple, just inject service to check
+    canActivate: [() => inject(LoginService).isLoggedIn()],
+  },
+];
+
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserAnimationsModule)]
-})
-  .catch(err => console.error(err));
+  providers: [importProvidersFrom(RouterModule.forRoot(routes))],
+}).catch((err) => console.error(err));
